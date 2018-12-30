fcad7a8425204c773d090c56207ad624305dad26
Phill Conrad <pconrad@cs.ucsb.edu>
2018-12-30 09:21:41 -0800

    convert to new format

--- jekyll.sh
+++ jekyll.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm"
-[[ -s /etc/profile.d/rvm.sh" ]] && source "/etc/profile.d/rvm.sh"
+[[ -s "/etc/profile.d/rvm.sh" ]] && source "/etc/profile.d/rvm.sh"
 
 rvm use 2.5.1
 bundle exec jekyll serve
