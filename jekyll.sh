2ae3e41b7d355fa36420f5f435f89f7d8523e1c4
Phill Conrad <pconrad@cs.ucsb.edu>
2019-12-19 14:51:03 -0800

    pc - update boilerplate

--- jekyll.sh
+++ jekyll.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
+
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm"
-[[ -s "/etc/profile.d/rvm.sh" ]] && source "/etc/profile.d/rvm.sh"
-
-rvm use 2.5.1
+rvm use 2.5.3
 bundle exec jekyll serve $@
-
--- setup.sh
+++ setup.sh
@@ -3,11 +3,12 @@
 echo "Installing software needed to run Jekyll locally... "
 
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm"
-[[ -s "/etc/profile.d/rvm.sh" ]] && source "/etc/profile.d/rvm.sh"
 
-
-rvm install ruby-2.5.1
-rvm use 2.5.1
-gem install bundler
+rvm install ruby-2.5.3
+rvm use 2.5.3
+gem install bundler 
+gem install pkg-config
+gem install nokogiri
+# bundle config build.nokogiri --use-system-libraries
 bundle install --path vendor/bundle
 echo "Done."
